a7a8cb788484e960affcac5d2797e81cb9c27e11
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-12-17 02:12:26 +0600

    user data

--- index.js
+++ index.js
@@ -16,6 +16,45 @@ const client = new MongoClient(uri, {
    useUnifiedTopology: true,
 });
 
+async function run() {
+   try {
+      await client.connect();
+      console.log("connected to db");
+
+      const database = await client.db("job_sites_data");
+      const userCollection = database.collection("users");
+
+      //  user GET, POST, PUT
+      app.route("/users")
+         .get(async (req, res) => {
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+         })
+         .post(async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.json(result);
+         })
+         .put(async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await userCollection.updateOne(
+               filter,
+               updateDoc,
+               options
+            );
+            res.json(result);
+         });
+   } finally {
+      // await client.close();
+   }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
    res.send("Try Out server running");
 });
